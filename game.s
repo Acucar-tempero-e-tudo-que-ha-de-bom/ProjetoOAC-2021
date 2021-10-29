53cf384de5114509811cce6c5df6b580d15a0068
davipatury <davi.patury@yahoo.com.br>
2021-10-29 13:35:04 -0300

    game.s tab tab tab

--- game.s
+++ game.s
@@ -28,38 +28,38 @@ INPUT_ZEROES:	.byte 0		# may be temporary
 
 .text		
 		# Open MAPA file
-		li a7,1024
-		la a0,FILE_MAP
-		li a1,0
+		li		a7, 1024
+		la		a0, FILE_MAP
+		li		a1, 0
 		ecall
-		mv s0,a0
+		mv		s0, a0
 		
 		# Open CHAR file
-		la a0,FILE_CHAR
+		la		a0, FILE_CHAR
 		ecall
-		mv s2,a0
+		mv		s2, a0
 		
 		# Open DEBUG file
-		la a0,FILE_DEBUG
+		la		a0, FILE_DEBUG
 		ecall
-		mv s9,a0
+		mv		s9, a0
 
-		li s1,1
+		li		s1, 1
 		
-		la t0,CHAR_POS
-		flw fs0,0(t0)		# fs0 = char x
-		flw fs1,4(t0)		# fs1 = char y
+		la		t0, CHAR_POS
+		flw		fs0, 0(t0)		# fs0 = char x
+		flw		fs1, 4(t0)		# fs1 = char y
 
-		fcvt.s.w fs2,zero	# fs2 = x velocity
-		fcvt.s.w fs3,zero	# fs3 = y velocity
-		fcvt.s.w fs4,zero	# fs4 = jump grace timer
-		fcvt.s.w fs5,zero	# fs5 = varJumpTimer
-		fcvt.s.w fs6,zero	# fs6 = varJumpSpeed
-		fcvt.s.w fs7,zero	# fs7 = maxfall
-		fcvt.s.w fs8,zero	# fs8 = dash timer
+		fcvt.s.w	fs2, zero		# fs2 = x velocity
+		fcvt.s.w	fs3, zero		# fs3 = y velocity
+		fcvt.s.w	fs4, zero		# fs4 = jump grace timer
+		fcvt.s.w	fs5, zero		# fs5 = varJumpTimer
+		fcvt.s.w	fs6, zero		# fs6 = varJumpSpeed
+		fcvt.s.w	fs7, zero		# fs7 = maxfall
+		fcvt.s.w	fs8, zero		# fs8 = dash timer
 		
-		li t0,MAX_FALL
-		fcvt.s.w fs7,t0		# fs7 = max fall
+		li		t0, MAX_FALL
+		fcvt.s.w	fs7, t0			# fs7 = max fall
 
 		#
 		# Registradores que devem permanecer durante o loop
@@ -80,127 +80,127 @@ GAME_LOOP:	#
 		# O framerate atual do jogo e 60 fps, mas com o passar do desenvolvimento, se o
 		# jogo ficar muito pesado, pode ser interessante diminuir esse framerate.
 		#
-		csrr t0,3073			# t0 = current time
-		sub t0,t0,s11			# t0 = current time - last frame time
-		li t1,16			# 16ms entre cada frame (1000ms/60fps)
-		bltu t0,t1,GAME_LOOP		# enquanto n tiver passado 16ms, repete
+		csrr		t0, 3073		# t0 = current time
+		sub		t0, t0, s11		# t0 = current time - last frame time
+		li		t1, 16			# 16ms entre cada frame (1000ms/60fps)
+		bltu		t0, t1, GAME_LOOP	# enquanto n tiver passado 16ms, repete
 
-		li t1,1000
-		fcvt.s.w ft0, t1                # ft0 = 1000
-		fcvt.s.w fa7, t0                # fa7 = delta time (in ms)
-		fdiv.s fa7, fa7, ft0            # fa7 /= 1000 (delta time in seconds)
+		li		t1, 1000
+		fcvt.s.w	ft0, t1			# ft0 = 1000
+		fcvt.s.w	fa7, t0			# fa7 = delta time (in ms)
+		fdiv.s		fa7, fa7, ft0		# fa7 /= 1000 (delta time in seconds)
 		
-		call PHYSICS
-		call INPUT
+		call		PHYSICS
+		call		INPUT
 	
 		# Calcular posicao do mapa de acordo com o personagem
 		# O personagem deve ficar sempre que possível no centro da tela
 
 		# Calculo do x
-		fcvt.w.s a0,fs0			# a0 = char x
+		fcvt.w.s	a0, fs0			# a0 = char x
 
-		addi a0,a0,MAP_OFFSET_X		# a0 = char x - offset x do mapa (o mapa fica x pixels pra esquerda do personagem)
-		mv a1,zero			# a1 = 0
-		call MAX			# faz um MAX entre o resultado da conta e 0 (garante que o x do mapa seja >= 0)
+		addi		a0, a0, MAP_OFFSET_X	# a0 = char x - offset x do mapa (o mapa fica x pixels pra esquerda do personagem)
+		mv		a1, zero		# a1 = 0
+		call		MAX			# faz um MAX entre o resultado da conta e 0 (garante que o x do mapa seja >= 0)
 		
-		li a1,MAP_MAX_X			# a1 = maximo que o mapa pode ir no eixo X
-		call MIN			# faz um MIN entre o resultado da conta e o MAXIMO que o mapa pode ir no eixo X
+		li		a1, MAP_MAX_X		# a1 = maximo que o mapa pode ir no eixo X
+		call		MIN			# faz um MIN entre o resultado da conta e o MAXIMO que o mapa pode ir no eixo X
 		
-		mv s3,a0			# move o resultado pra s3
+		mv		s3, a0			# move o resultado pra s3
 		
 		# Calculo do y
-		fcvt.w.s a0,fs1			# a0 = char y
-		addi a0,a0,MAP_OFFSET_Y		# a0 = char y - offset y do mapa (o mapa fica x pixels pra cima do personagem)
-		mv a1,zero			# a1 = zero
-		call MAX			# faz um MAX entre o resultado da conta e 0 (garante que o y do mapa seja >= 0)
+		fcvt.w.s		a0, fs1		# a0 = char y
+		addi		a0, a0, MAP_OFFSET_Y	# a0 = char y - offset y do mapa (o mapa fica x pixels pra cima do personagem)
+		mv		a1, zero		# a1 = zero
+		call		MAX			# faz um MAX entre o resultado da conta e 0 (garante que o y do mapa seja >= 0)
 		
-		li a1,MAP_MAX_Y			# a1 = maximo que o mapa pode ir no eixo Y
-		call MIN			# faz um MIN entre o resultado da conta e o MAXIMO que o mapa pode ir no eixo Y
+		li		a1, MAP_MAX_Y		# a1 = maximo que o mapa pode ir no eixo Y
+		call		MIN			# faz um MIN entre o resultado da conta e o MAXIMO que o mapa pode ir no eixo Y
 		
-		mv s4,a0			# move o resultado pra s4
+		mv		s4, a0			# move o resultado pra s4
 
 		# Define os argumentos a0-a5 e desenha o mapa
 		# os calculos pros argumentos a6-a7 são definidos acima
-		mv a0,s0
-		li a1,0
-		li a2,0
-		la a3,FILE_MAP_SIZE
-		la a4,SCREEN_SIZE
-		mv a5,s1
-		mv a6,s3
-		mv a7,s4
-		call RENDER
+		mv		a0, s0
+		li		a1, 0
+		li		a2, 0
+		la		a3, FILE_MAP_SIZE
+		la		a4, SCREEN_SIZE
+		mv		a5, s1
+		mv		a6, s3
+		mv		a7, s4
+		call		RENDER
 		
 		# Draw char
-		mv a0,s2
+		mv		a0, s2
 		
 		# Calculo da posicao do personagem na tela em relacao ao mapa
 
 		# x = char x - map x
-		fcvt.w.s a1,fs0
-		sub a1,a1,s3
+		fcvt.w.s	a1, fs0
+		sub		a1, a1, s3
 		
 		# y = char y - map y
-		fcvt.w.s a2,fs1
-		sub a2,a2,s4
+		fcvt.w.s	a2, fs1
+		sub		a2, a2, s4
 		
-		la a3,FILE_CHAR_SIZE
-		la a4,FILE_CHAR_DRAW
-		mv a5,s1
+		la		a3, FILE_CHAR_SIZE
+		la		a4, FILE_CHAR_DRAW
+		mv		a5, s1
 		
-		la t0, CHAR_DIR
-		lb t0, 0(t0)
+		la		t0, CHAR_DIR
+		lb		t0, 0(t0)
 		
-		li t1, 32
-		mul a6, t0, t1
+		li		t1, 32
+		mul		a6, t0, t1
 		
-		li a7,0
-		call RENDER
+		li		a7, 0
+		call		RENDER
 		
 		# Draw DEBUG
-		mv a0,s9
+		mv		a0, s9
 		
-		li t0, 80
-		li t1, 8
-		rem a1, s10, t0
-		mul a1, a1, t1
-		sub a1, a1, s3
+		li		t0, 80
+		li		t1, 8
+		rem		a1, s10, t0
+		mul		a1, a1, t1
+		sub		a1, a1, s3
 
-		div a2, s10, t0
-		mul a2, a2, t1
-		sub a2, a2, s4
+		div		a2, s10, t0
+		mul		a2, a2, t1
+		sub		a2, a2, s4
 		
-		la a3,FILE_DEBUG_SIZE
-		la a4,FILE_DEBUG_SIZE
-		mv a5,s1
-		li a6,0
-		li a7,0
-		call RENDER
+		la		a3, FILE_DEBUG_SIZE
+		la		a4, FILE_DEBUG_SIZE
+		mv		a5, s1
+		li		a6, 0
+		li		a7, 0
+		call		RENDER
 		
 		# Salva quando esse frame terminou de renderizar
 		# Usado pra garantir um framerate fixo no jogo
-		csrr s11,3073
+		csrr		s11, 3073
 		
 		# Troca de frame
 		# Agora que esta tudo renderizado pode mostrar pro usuario
-		li t0,FRAME_CONTROL_ADDRESS
-		sw s1,0(t0)
-		xori s1,s1,1
+		li		t0, FRAME_CONTROL_ADDRESS
+		sw		s1, 0(t0)
+		xori		s1, s1, 1
 		
 		j GAME_LOOP
 		
 EXIT:		# Closes MAPA file
-		li a7,57
-		mv a0,s0
+		li		a7, 57
+		mv		a0, s0
 		ecall
 		
 		# Closes CHAR file
-		li a7,57
-		mv a0,s2
+		li		a7, 57
+		mv		a0, s2
 		ecall
 
 		# Exit
-		li a7,10
+		li		a7, 10
 		ecall
 
 .include "helpers/input.s"
